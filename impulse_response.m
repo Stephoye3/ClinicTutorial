d2b53a95d4f68a028d05e6fd4117033b7b566fcb
Stephanie C <sclifner@hmc.edu>
2020-02-02 23:38:28 +0000

    Fixed frequency response calculation

--- impulse_response.m
+++ impulse_response.m
@@ -1,3 +1,4 @@
+% Stephanie Clifner 2/2/2020
 function [ h,t ] = impulse_response( x, y, fs )
 %impulse_response returns the impulse response of the system given the
 %system input, x, and system output, y.
@@ -8,8 +9,9 @@ function [ h,t ] = impulse_response( x, y, fs )
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = Y./X;   % the calculated frequency response
 [h,t] = tdomain(H,fs);
+plot(t,h)
 
 end
 
